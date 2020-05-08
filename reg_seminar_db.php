4e726022ec9f77033bff28021c61f4cb30452c66
axzessjr <axzess@gmail.com>
2020-05-08 21:15:00 +0700

    Update reg_seminar_db.php

--- reg_seminar_db.php
+++ reg_seminar_db.php
@@ -4,11 +4,13 @@
 
 if (isset($_POST['reg_seminar'])) {
     $reg_type = mysqli_real_escape_string($connect, $_POST['reg_type']);
+    $institue = mysqli_real_escape_string($connect, $_POST['institute']);
     $attendance_type = mysqli_real_escape_string($connect, $_POST['attendance_type']);
     
-    $sql = "INSERT INTO register VALUES ('','$_SESSION[user_id]','$institue','$reg_type','$attendance_type',)";
+    $sql = "INSERT INTO register (user_id, institute, register_type, attendance_type) 
+            VALUES ('$_SESSION[user_id]','$institue','$reg_type','$attendance_type')";
     
     
-    mysqli_query($connect, $sql);
+    mysqli_query($connect, $sql) or die("SQL errors: " . mysqli_error($connect));
 }
 ?>
