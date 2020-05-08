25c1e2da4284534de2cd0ba01e6d90a18d139e4c
axzessjr <axzess@gmail.com>
2020-05-08 14:39:39 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer id="footer">
+    <div class="container">
+      <h3>Thank you For Visiting Our Website</h3><br>
+      <h4>Contact Us</h4>
+      <div class="social-links">
+        <a href="" class="facebook"><i class="fab fa-facebook"></i></a>
+        <a href="" class="phone"><i class="fas fa-phone"></i></a>
+        <a href="" class="lie"><i class="fab fa-line"></i></a>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+</body>
+
+</html>
