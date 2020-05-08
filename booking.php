c1f6a4124ab145c3769a2ac004f9b884dca8a7eb
nattaln <55180766+nattaln@users.noreply.github.com>
2020-05-08 13:27:37 +0700

    Update booking.php

--- booking.php
+++ booking.php
@@ -4,18 +4,23 @@
 </head>
 <body>
 <?php
+//variable
 $firstname = $_GET['firstname'];
 $lastname = $_GET['lastname'];
 $room_type = $_GET['room_type'];
 $bed_type = $_GET['bed_type'];
 $brakefast = $_GET['r1'];
 $room_counts = $_GET['room_counts'];
+$register_id = '2';
+$hotel_id = '1';
 
+//connectdb
 include_once("connectdb.php");
 
-$sql = "INSERT INTO booking (booking_id, register_id, hotel_id, firstname, lastname, room_counts) VALUES ( '','','' ,'$firstname' ,'$lastname' ,'$room_counts')";
-$dbInsert = mysqli_query($dbLocalhost, $sql)
-    or die("Problem reading table: " . mysqli_error($dbLocalhost));
+//insert booking
+$sql = "INSERT INTO booking (booking_id, register_id, hotel_id, firstname, lastname, room_counts, breakfast_included) VALUES ( '','$register_id','$hotel_id' ,'$firstname' ,'$lastname' ,'$room_counts', '$brakefast')";
+$dbInsert = mysqli_query($connect, $sql)
+    or die("Problem reading table: " . mysqli_error($connect));
 if($dbInsert){
     echo "Insert successful ";
 }
@@ -23,15 +28,20 @@ else {
     echo "Insert not successful ";
 }
 
-$sql1 = "INSERT INTO room (room_id, room_type, bed_type, breakfast_included) VALUES ( '','$room_type','$bed_type' ,'$brakefast')";
-$dbInsert1 = mysqli_query($dbLocalhost, $sql1)
-    or die("Problem reading table: " . mysqli_error($dbLocalhost));
+//insert room
+$sql1 = "INSERT INTO room (room_id, room_number, room_type, bed_type, hotel_id) VALUES ( '','','$room_type','$bed_type' ,'$hotel_id')";
+$dbInsert1 = mysqli_query($connect, $sql1)
+    or die("Problem reading table: " . mysqli_error($connect));
 if($dbInsert1){
     echo "Insert successful ";
 }
 else {
     echo "Insert not successful ";
 }
+
+
+//test
+echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
 
 ?>
 </body>
