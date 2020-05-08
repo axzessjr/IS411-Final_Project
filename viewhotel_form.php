d61a5cc556b07eb41e493aef0c1fada3fa794adb
wolf20099 <54096364+wolf20099@users.noreply.github.com>
2020-05-08 16:02:23 +0700

    Update viewhotel_form.php

--- viewhotel_form.php
+++ viewhotel_form.php
@@ -1,7 +1,7 @@
   
 <?php include "header.php" ?>
     <div class="login-form">
-    <h2 class="text-center">View Hotel</h2>
+    <h2 class="text-center">Hotel Details</h2>
     <form method="GET" action="view_hotel.php">
         Reg ID: <INPUT TYPE="text" NAME="register_id" class="form-control" ><br/>        
         <INPUT TYPE="submit" name='submit' class="btn btn-primary btn-block" >
