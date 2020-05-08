fcc5cd1ed708d9acd3f34aadd68411d16369e64d
axzessjr <axzess@gmail.com>
2020-05-08 13:49:31 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
-    include('connectdb.php');
+    //include('connectdb.php');
+
+    $connect = mysqli_connect("localhost", "root", "root", "seminardb") or die("Could not connect: " . mysqli_connect_errno());
 
     if (isset($_POST['login_user'])){
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $password = md5($password);
         $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
         $check = mysqli_query($connect, $sql);
         $numrows = mysqli_num_rows($check);
