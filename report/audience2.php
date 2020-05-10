a0fb224ee3265197fca4d07853c5d02663bbc8cc
PoyRamona <65004535+PoyRamona@users.noreply.github.com>
2020-05-11 00:51:49 +0700

    Add files via upload

---
+++ report/audience2.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!file_exists("report/audience2.csv"))
+    $fileTextFile1 = fopen ("report/audience2.csv",'w');
+else
+    $fileTextFile1 = fopen ("report/audience2.csv",'a');
+
+fwrite($fileTextFile1,"Audience\r\n");
+fwrite($fileTextFile1,"UserID,Firstname,Lastname,Institue\r\n");
+   
+include "connectdb.php";
+$SQL = "SELECT * FROM user,register WHERE user.user_id = register.user_id AND attendance_type = 'audience' AND register_type ='Early bird without workshops'"; 
+$dbRecords = mysqli_query($connect,$SQL);
+$numrows = mysqli_num_rows($dbRecords);
+$sum=0;
+for ($i=0; $i < $numrows; $i++) { 
+$arrRecord = mysqli_fetch_array($dbRecords);
+    fwrite($fileTextFile1, $arrRecord[user_id]);
+    fwrite($fileTextFile1, ",");
+    fwrite($fileTextFile1, $arrRecord[firstname]);
+    fwrite($fileTextFile1, ",");
+    fwrite($fileTextFile1, $arrRecord[lastname]);
+    fwrite($fileTextFile1, ",");
+    fwrite($fileTextFile1, $arrRecord[institue]);
+    fwrite($fileTextFile1, "\r\n");
+    $sum=$sum+1;
+}
+
+fwrite($fileTextFile1, "\r\n");
+fwrite($fileTextFile1," Total, $sum");
+fwrite($fileTextFile1, "\r\n");
+
+if (!fclose($fileTextFile1))
+    echo "<p>Error closing file!</p>";
+ 
+?>
