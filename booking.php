47df6666528a813861c652d96812301470280e1b
nattaln <55180766+nattaln@users.noreply.github.com>
2020-05-11 15:37:53 +0700

    Update booking.php

--- booking.php
+++ booking.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>booking</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
 <?php
@@ -12,6 +13,17 @@
     }
 ?>
 <?php
+//connectdb
+include_once("connectdb.php");
+
+
+$sql1 = "SELECT * FROM user, register WHERE user.user_id = register.user_id AND email = '$_SESSION[email]'";
+$query = mysqli_query($connect, $sql1); 
+
+ while ($arrRecords = mysqli_fetch_array($query)) {
+             $_SESSION['reg_id'] = $arrRecords["register_id"];
+      }
+
 //variable
 $firstname = $_GET['firstname'];
 $lastname = $_GET['lastname'];
@@ -19,11 +31,9 @@ $room_type = $_GET['room_type'];
 $bed_type = $_GET['bed_type'];
 $brakefast = $_GET['r1'];
 $room_counts = $_GET['room_counts'];
-$register_id = '1';
+$register_id = $_SESSION['reg_id'];
 $hotel_id = '1';
 
-//connectdb
-include_once("connectdb.php");
 
 //insert booking
 $sql = "INSERT INTO booking (register_id, hotel_id, firstname, lastname, room_counts, breakfast_included) 
@@ -34,24 +44,20 @@ $sql = "INSERT INTO booking (register_id, hotel_id, firstname, lastname, room_co
     $dbInsert = mysqli_query($connect, $sql)
     or die("Problem reading table: " . mysqli_error($connect));
 
-if($dbInsert){
-    echo "Insert successful ";
-}
-else {
-    echo "Insert not successful ";
-}
-
 //insert room
-
 $sql1 = "INSERT INTO room (room_type, bed_type, hotel_id) VALUES ( '$room_type','$bed_type' ,'$hotel_id')";
 
 $dbInsert1 = mysqli_query($connect, $sql1)
     or die("Problem reading table: " . mysqli_error($connect));
-if($dbInsert1){
+
+//insert result
+if($dbInsert1 && $dbInsert){
     echo "Insert successful ";
+    echo "<a href='index.php' class='w3-button w3-black'>กลับสู่หน้าหลัก</a>";
 }
 else {
     echo "Insert not successful ";
+    echo "<a href='index.php' class='w3-button w3-black'>จองที่พักใหม่</a>";
 }
 
 
