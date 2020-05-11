2ddf644e51ef488edf6f7f6c87298c8ec832c476
yuppry <55180825+yuppry@users.noreply.github.com>
2020-05-11 21:56:05 +0700

    Update getfee.php

--- getfee.php
+++ getfee.php
@@ -1,12 +1,13 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <?php
     include("connectdb.php");
-    include("header.php");
     $reg_type = $_GET['rt'];
     $sql = "SELECT fee FROM register_type WHERE register_type = '$reg_type'";
     $query = mysqli_query($connect,$sql);
     $fee = mysqli_fetch_row($query);
-    echo "<h3>";
+    echo "Register fee : ";
+    echo "<span class='badge badge-primary'><h6>";
     echo $fee[0];
     echo " THB";
-    echo "</h3>";
+    echo "</span></h6>";
 ?>
